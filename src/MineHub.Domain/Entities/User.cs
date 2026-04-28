1e1ad61ccded53dc1ff10f7f2de775d769633bd6
vivaldistone <pipenabsolute@icloud.com>
2026-04-28 23:36:23 +0300

    feat(user) : Add user entity with minecraft profile value object

---
+++ src/MineHub.Domain/Entities/User.cs
@@ -0,0 +1,52 @@
+﻿using MineHub.Domain.ValueObjects;
+using MineHub.Domain.Exceptions;
+
+namespace MineHub.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; private set; }
+    public string AuthUserId { get; private set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+    public DateTime CreatedAtUtc { get; private set; }
+    public MinecraftProfile? MinecraftProfile { get; private set; }
+
+    private User() { }
+
+    public User(string authUserId, string email)
+    {
+        if (string.IsNullOrWhiteSpace(authUserId))
+            throw new DomainException("Auth User Id is required", "invalid_identity_user_id");
+        if (string.IsNullOrWhiteSpace(email))
+            throw new DomainException("User Email is required", "invalid_user_email");
+
+        Id = Guid.NewGuid();
+        AuthUserId = authUserId.Trim();
+        Email = email.Trim();
+        CreatedAtUtc = DateTime.UtcNow;
+    }
+
+    public void ChangeEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            throw new DomainException("User Email is required", "invalid_user_email");
+
+        Email = email.Trim();
+    }
+
+    public void LinkMinecraftProfile(MinecraftProfile profile)
+    {
+        if (profile is null)
+            throw new DomainException("Minecraft Profile is required", "invalid_minecraft_profile");
+
+        MinecraftProfile = profile;
+    }
+
+   public void UnlinkMinecraftProfile()
+   {
+        if (MinecraftProfile is null)
+            throw new DomainException("Minecraft Profile is not linked", "invalid_unlink_profile");
+        
+        MinecraftProfile = null;
+   }
+}
---
+++ src/MineHub.Domain/ValueObjects/MinecraftProfile.cs
@@ -0,0 +1,20 @@
+﻿using MineHub.Domain.Exceptions;
+
+namespace MineHub.Domain.ValueObjects;
+
+public sealed record MinecraftProfile
+{
+    public Guid MinecraftUuid { get;}
+    public string NickName { get;} = string.Empty;
+
+    public MinecraftProfile(Guid minecraftUuid, string nickName)
+    {
+        if (minecraftUuid == Guid.Empty)
+            throw new DomainException("minecraftUuid is required", "invalid_minecraft_uuid");
+        if (String.IsNullOrWhiteSpace(nickName))
+            throw new DomainException("NickName is required", "invalid_nickname");
+
+        MinecraftUuid = minecraftUuid;
+        NickName = nickName.Trim();
+    }
+}
