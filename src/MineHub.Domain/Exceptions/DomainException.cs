1a64ba416356417f4df6f6e40d4dac9f0e1f838b
vivaldistone <pipenabsolute@icloud.com>
2026-04-28 22:28:06 +0300

    feat(domain): add DomainException class

---
+++ src/MineHub.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace MineHub.Domain.Exceptions;
+
+public class DomainException : Exception
+{
+    public string Code { get;}
+    public DomainException(string message, string code) : base(message)
+    {
+        Code = code;
+    }
+}
