6810806bbabb887ed11ef4db0ffdaf1464807be1
vivaldistone <pipenabsolute@icloud.com>
2026-05-03 22:10:45 +0300

    feat(ConflictException) : Add Conflict Exception class

---
+++ src/MineHub.Application/Exceptions/ConflictException.cs
@@ -0,0 +1,11 @@
+﻿namespace MineHub.Application.Exceptions;
+
+public class ConflictException : Exception
+{
+    public string Code { get; private set; }
+
+    public ConflictException(string message, string code) : base(message)
+    {
+        Code = code;
+    }
+}
