629a8a3b7d05a534883eae3bc8fc237ea46f15e2
vivaldistone <pipenabsolute@icloud.com>
2026-05-03 21:40:14 +0300

    feat(products) : Add get products query handler

---
+++ src/MineHub.Application/Products/Queries/GetProducts/GetProductResponse.cs
@@ -0,0 +1,5 @@
+﻿using MineHub.Domain.Entities;
+
+namespace MineHub.Application.Products.Queries.GetProducts;
+
+public record GetProductsResponse(Guid ProductId, string Name, string Description, decimal Price);
---
+++ src/MineHub.Application/Products/Queries/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MineHub.Application.Abstractions.Persistence;
+
+namespace MineHub.Application.Products.Queries.GetProducts;
+
+public class GetProductsQueryHandler
+{
+    private readonly IProductRepository _productRepository;
+
+    public GetProductsQueryHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+    public async Task<IReadOnlyCollection<GetProductResponse>> HandleAsync()
+    {
+        var products = await _productRepository.GetAllAsync();
+
+        return products.Select(p =>
+        new GetProductResponse(
+            p.ProductId,
+            p.Name,
+            p.Description,
+            p.Price))
+            .ToList()
+            .AsReadOnly();
+    }
+}
