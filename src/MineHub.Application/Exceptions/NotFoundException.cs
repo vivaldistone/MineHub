720ddee9432eddcc3c18717291cd9c83d9f9ff56
vivaldistone <pipenabsolute@icloud.com>
2026-05-03 21:25:24 +0300

    feat(NotFoundException) : Add NotFoundException class

---
+++ src/MineHub.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace MineHub.Application.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public string Code { get; private set; }
+    public NotFoundException(string message, string code) : base(message)
+    {
+        Code = code;
+    }
+}
