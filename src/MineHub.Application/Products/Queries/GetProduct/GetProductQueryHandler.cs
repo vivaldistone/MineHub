4269ec99470bb57b1a2f7be45df1ce6952bc3cbf
vivaldistone <pipenabsolute@icloud.com>
2026-05-03 21:26:25 +0300

    feat(products): add get product query handler

---
+++ src/MineHub.Application/Products/Queries/GetProduct/GetProductQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MineHub.Application.Abstractions.Persistence;
+using MineHub.Application.Exceptions;
+
+namespace MineHub.Application.Products.Queries.GetProduct;
+
+public class GetProductQueryHandler
+{
+    private readonly IProductRepository _productRepository;
+
+    public GetProductQueryHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+
+    public async Task<GetProductResponse> HandleAsync(Guid id)
+    {
+        if (id == Guid.Empty)
+            throw new ArgumentException("Product id is required", nameof(id));
+        
+        var product = await _productRepository.GetByIdAsync(id);
+
+        if (product is null)
+            throw new NotFoundException("Product was not found", "product_not_found");
+
+        return new GetProductResponse(
+            product.ProductId,
+            product.Name,
+            product.Description,
+            product.Price);
+    }
+}
---
+++ src/MineHub.Application/Products/Queries/GetProduct/GetProductResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace MineHub.Application.Products.Queries.GetProduct;
+
+public record GetProductResponse(Guid ProductId, string Name, string Description, decimal Price);
