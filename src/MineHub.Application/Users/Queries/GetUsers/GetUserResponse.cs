295040c67c93cefcc5cf0e2fa8ba4009c8d9078a
vivaldistone <pipenabsolute@icloud.com>
2026-05-04 18:35:07 +0300

    feat(users) : add get users query handler

---
+++ src/MineHub.Application/Users/Queries/GetUsers/GetUserResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace MineHub.Application.Users.Commands;
+
+public record GetUserResponse(Guid Id, string Email, DateTime CreatedAtUtc, Guid? MinecraftUuid, string? MinecraftName);
---
+++ src/MineHub.Application/Users/Queries/GetUsers/GetUsersQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MineHub.Application.Abstractions.Persistence;
+
+namespace MineHub.Application.Users.Queries.GetUsers;
+
+public class GetUsersQueryHandler
+{
+    private readonly IUserRepository _userRepository;
+
+    public GetUsersQueryHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<IReadOnlyCollection<GetUserResponse>> HandleAsync()
+    {
+        var users = await _userRepository.GetAllAsync();
+
+        return users.Select(u => new GetUserResponse(
+            u.Id,
+            u.Email,
+            u.CreatedAtUtc,
+            u.MinecraftProfile?.MinecraftUuid,
+            u.MinecraftProfile?.NickName))
+            .ToList();
+    }
+}
