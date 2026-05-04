21045a01ab87935be0cc4166d132c59d9d06ab88
vivaldistone <pipenabsolute@icloud.com>
2026-05-04 23:15:13 +0300

    feat(auth) : implement JwtTokenGenerator class

---
+++ src/MineHub.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,52 @@
+﻿using MineHub.Application.Abstractions.Services;
+using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace MineHub.Infrastructure.Authentication
+{
+    internal class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(string userId, string email, IEnumerable<string> roles)
+        {   
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId),
+                new Claim(JwtRegisteredClaimNames.Email, email),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var secretKey = _configuration["Jwt:SecretKey"];
+
+            if (String.IsNullOrWhiteSpace(secretKey))
+                throw new InvalidOperationException("Jwt Secret Key is missing");
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "MineHub",
+                audience: "MineHub-client",
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(15),
+                signingCredentials: credentials
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
