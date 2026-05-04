0ba627901ee68ef788b90022f72d708fa692d177
vivaldistone <pipenabsolute@icloud.com>
2026-05-04 23:12:45 +0300

    feat(auth) : Add IJwtTokenGenerator interface

---
+++ src/MineHub.Application/Abstractions/Services/IJwtTokenGenerator.cs
@@ -0,0 +1,6 @@
+﻿namespace MineHub.Application.Abstractions.Services;
+
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(string userId, string email, IEnumerable<string> roles); 
+}
