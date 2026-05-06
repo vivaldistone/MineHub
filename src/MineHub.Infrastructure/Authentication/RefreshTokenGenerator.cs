480da13f84764309bd81d32336d3461db98e3cd9
vivaldistone <pipenabsolute@icloud.com>
2026-05-06 19:26:06 +0300

    feat(auth) : add refresh token generator

---
+++ src/MineHub.Infrastructure/Authentication/RefreshTokenGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+using MineHub.Application.Abstractions.Services;
+
+namespace MineHub.Infrastructure.Authentication;
+
+public class CryptographicStringGenerator : ICryptographicStringGenerator
+{
+    public string Generate(int sizeInBytes = 64)
+    {
+        var bytes = RandomNumberGenerator.GetBytes(sizeInBytes);
+
+        return Convert.ToBase64String(bytes);
+    }
+}
