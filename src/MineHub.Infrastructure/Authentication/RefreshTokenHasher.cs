164dbd2f3ea7c53017ec5ce5d854ecf1155b9399
vivaldistone <pipenabsolute@icloud.com>
2026-05-06 19:27:51 +0300

    feat(auth) : add refresh token hasher

---
+++ src/MineHub.Infrastructure/Authentication/RefreshTokenHasher.cs
@@ -0,0 +1,15 @@
+﻿using MineHub.Application.Abstractions.Services;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MineHub.Infrastructure.Authentication;
+
+public class RefreshTokenHasher : IRefreshTokenHasher
+{
+    public string Hash(string refreshToken)
+    {       
+        var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(refreshToken));
+
+        return Convert.ToBase64String(bytes);
+    }
+}
