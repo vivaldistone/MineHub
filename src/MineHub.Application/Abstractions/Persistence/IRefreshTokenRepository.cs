6000eb959350e9bb04e525ce2bf34a4185c355ae
vivaldistone <pipenabsolute@icloud.com>
2026-05-07 19:45:13 +0300

    feat(auth) : Add RefreshTokenRepository abstractions

---
+++ src/MineHub.Application/Abstractions/Persistence/IRefreshTokenRepository.cs
@@ -0,0 +1,10 @@
+﻿using MineHub.Domain.Entities;
+
+namespace MineHub.Application.Abstractions.Persistence;
+
+public interface IRefreshTokenRepository
+{
+    Task AddAsync(RefreshToken refreshToken);
+    Task<RefreshToken> GetRefreshTokenAsync(string hash);
+    Task UpdateAsync(RefreshToken token);
+}
