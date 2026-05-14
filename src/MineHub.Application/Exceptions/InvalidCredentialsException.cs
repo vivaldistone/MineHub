b30eb8884c039eaf4b15d1d03d073a7c6d1e2581
vivaldistone <pipenabsolute@icloud.com>
2026-05-14 18:17:24 +0300

    feat(InvalidCredentialsException): add invalid credentials exception

---
+++ src/MineHub.Application/Exceptions/InvalidCredentialsException.cs
@@ -0,0 +1,10 @@
+﻿namespace MineHub.Application.Exceptions;
+
+public class InvalidCredentialsException : Exception
+{
+    public string StatusCode { get; private set; }
+    public InvalidCredentialsException(string message, string statusCode) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
