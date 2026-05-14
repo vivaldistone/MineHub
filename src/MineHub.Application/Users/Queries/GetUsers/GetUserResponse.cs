f4cbd046426548505b64278a1fbdcef8007480ed
vivaldistone <pipenabsolute@icloud.com>
2026-05-14 18:14:10 +0300

    feat(users) : add get user response

--- src/MineHub.Application/Users/Queries/GetUsers/GetUserResponse.cs
+++ src/MineHub.Application/Users/Queries/GetUsers/GetUserResponse.cs
@@ -1,3 +1,3 @@
-﻿namespace MineHub.Application.Users.Commands;
+﻿namespace MineHub.Application.Users.Queries.GetUsers;
 
 public record GetUserResponse(Guid Id, string Email, DateTime CreatedAtUtc, Guid? MinecraftUuid, string? MinecraftName);
