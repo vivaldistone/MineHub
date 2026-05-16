89dc96cd7c93c0c8480ab1a8c7e14d3cabad4f68
vivaldistone <pipenabsolute@icloud.com>
2026-05-16 14:21:01 +0300

    feat(order) : add create order from cart

---
+++ src/MineHub.Application/Orders/Commands/CreateOrderFromCart/CreateOrderFromCartCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MineHub.Application.Abstractions.Persistence;
+using MineHub.Application.Abstractions.Services;
+using MineHub.Application.Exceptions;
+using MineHub.Domain.Entities;
+
+namespace MineHub.Application.Orders.Commands.CreateOrderFromCart;
+
+public class CreateOrderFromCartCommandHandler
+{
+    private readonly ICartRepository _cartRepository;
+    private readonly IOrderRepository _orderRepository;
+    private readonly ICurrentDomainUserService _currentDomainUserService;
+
+    public CreateOrderFromCartCommandHandler(ICartRepository cartRepository, IOrderRepository orderRepository, ICurrentDomainUserService currentDomainUserService)
+    {
+        _cartRepository = cartRepository;
+        _orderRepository = orderRepository;
+        _currentDomainUserService = currentDomainUserService;
+    }
+
+    public async Task HandleAsync()
+    {
+        var user = await _currentDomainUserService.GetRequiredAsync();
+
+        var cart = await _cartRepository.GetByUserIdAsync(user.Id)
+            ?? throw new NotFoundException("Cart not found", "cart_not_found");
+
+        var order = Order.Create(cart);
+        cart.Clear();
+
+        await _orderRepository.AddAsync(order);
+        await _cartRepository.UpdateAsync(cart);
+    }
+}
