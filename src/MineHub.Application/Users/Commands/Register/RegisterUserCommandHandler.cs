28785edde4a9e4680f7f03ae955320e6b1baa60e
vivaldistone <pipenabsolute@icloud.com>
2026-05-16 12:41:39 +0300

    add cart creation during registration user

--- src/MineHub.Application/Users/Commands/Register/RegisterUserCommandHandler.cs
+++ src/MineHub.Application/Users/Commands/Register/RegisterUserCommandHandler.cs
@@ -1,27 +1,35 @@
-﻿using MineHub.Application.Abstractions.Services;
+﻿using MineHub.Application.Abstractions.Persistence;
+using MineHub.Application.Abstractions.Services;
 using MineHub.Application.Exceptions;
+using MineHub.Domain.Entities;
 
 namespace MineHub.Application.Users.Commands.Register;
 
 public class RegisterUserCommandHandler
 {
     private readonly IIdentityService _identityService;
+    private readonly IUserRepository _userRepository;
+    private readonly ICartRepository _cartRepository;
 
-    public RegisterUserCommandHandler(IIdentityService identityService)
+
+    public RegisterUserCommandHandler(IIdentityService identityService, IUserRepository userRepository, ICartRepository cartRepository)
     {
         _identityService = identityService;
+        _userRepository = userRepository;
+        _cartRepository = cartRepository;
     }
 
     public async Task HandleAsync(RegisterUserCommand command)
     {
-        if (command is null) 
-            throw new ArgumentNullException(nameof(command));
-
         var result = await _identityService.CreateUserAsync(command.Email, command.Password);
 
         if (!result.Success)
-            throw new ConflictException("User name already exists", "user_already_exists");
+            throw new ConflictException("User already exists", "user_already_exists");
 
-        
+        var user = new User(result.UserId, command.Email);
+        await _userRepository.AddAsync(user);
+
+        var cart = new Cart(user.Id);
+        await _cartRepository.AddAsync(cart); 
     }
 }
