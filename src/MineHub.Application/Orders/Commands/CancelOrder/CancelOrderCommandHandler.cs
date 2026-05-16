ca7e03cdf294f1cf31e75e26a2f61122dcfe1c13
vivaldistone <pipenabsolute@icloud.com>
2026-05-16 18:56:57 +0300

    add pay order and cancel order use cases

---
+++ src/MineHub.Application/Orders/Commands/CancelOrder/CancelOrderCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MineHub.Application.Abstractions.Persistence;
+using MineHub.Application.Abstractions.Services;
+using MineHub.Application.Exceptions;
+
+namespace MineHub.Application.Orders.Commands.CancelOrder;
+
+public class CancelOrderCommandHandler
+{
+    private readonly ICurrentDomainUserService _currentDomainUserService;
+    private readonly IOrderRepository _orderRepository;
+
+    public CancelOrderCommandHandler(ICurrentDomainUserService currentDomainUserService, IOrderRepository orderRepository)
+    {
+        _currentDomainUserService = currentDomainUserService;
+        _orderRepository = orderRepository;
+    }
+
+    public async Task HandleAsync()
+    {
+        var user = await _currentDomainUserService.GetRequiredAsync();
+
+        var createdOrder = await _orderRepository.GetCreatedByUserIdAsync(user.Id);
+
+        if (createdOrder is null)
+            throw new NotFoundException("Created order not found", "created_order_not_found");
+
+        createdOrder.Cancel();
+        await _orderRepository.UpdateAsync(createdOrder);
+    }
+}
