c7d99cc7286fd1f93dd326d5e4b36c2558694a1f
vivaldistone <pipenabsolute@icloud.com>
2026-05-17 14:57:33 +0300

    feat(ProductRepository) : add product repository

---
+++ src/MineHub.Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MineHub.Application.Abstractions.Persistence;
+using MineHub.Domain.Entities;
+
+namespace MineHub.Infrastructure.Persistence.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly AppDbContext _appDbContext;
+
+    public ProductRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+
+    public async Task AddAsync(Product product)
+    {
+        await _appDbContext.Products.AddAsync(product);
+        await _appDbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Product product)
+    {
+        _appDbContext.Products.Remove(product);
+        await _appDbContext.SaveChangesAsync();
+    }
+
+    public async Task<bool> ExistsAsync(Guid id)
+    {
+        return await _appDbContext.Products.AnyAsync(p => p.ProductId == id);
+    }
+
+    public async Task<IReadOnlyCollection<Product>> GetAllAsync()
+    {
+        return await _appDbContext.Products.ToListAsync();
+    }
+
+    public async Task<Product?> GetByIdAsync(Guid id)
+    {
+        return await _appDbContext.Products.FirstOrDefaultAsync(p => p.ProductId == id);
+    }
+
+    public async Task<Product?> GetByNameAsync(string name)
+    {
+        return await _appDbContext.Products.FirstOrDefaultAsync(p => p.Name == name);
+    }
+
+    public async Task UpdateAsync(Product product)
+    {
+        _appDbContext.Products.Update(product);
+        await _appDbContext.SaveChangesAsync();
+    }
+}
