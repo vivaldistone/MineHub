db4d9934221f0a043e16cf5cdb1e7a531d1a38eb
vivaldistone <pipenabsolute@icloud.com>
2026-05-17 14:59:29 +0300

    feat(UserRepository) : add user repository

---
+++ src/MineHub.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MineHub.Application.Abstractions.Persistence;
+using MineHub.Domain.Entities;
+
+namespace MineHub.Infrastructure.Persistence.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AppDbContext _appDbContext;
+
+    public UserRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _appDbContext.DomainUsers.AddAsync(user);
+        await _appDbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(User user)
+    {
+        _appDbContext.DomainUsers.Remove(user);
+        await _appDbContext.SaveChangesAsync();
+    }
+
+    public async Task<bool> ExistsAsync(Guid id)
+    {
+        return await _appDbContext.DomainUsers.AnyAsync(u => u.Id == id);
+    }
+
+    public async Task<IReadOnlyCollection<User>> GetAllAsync()
+    {
+        return await _appDbContext.DomainUsers.ToListAsync();
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _appDbContext.DomainUsers.FirstOrDefaultAsync(u => u.Email ==  email);
+    }
+
+    public async Task<User?> GetByIdAsync(Guid guid)
+    {
+        return await _appDbContext.DomainUsers.FirstOrDefaultAsync(u => u.Id == guid);
+    }
+
+    public async Task<User> GetByIdentityUserIdAsync(string identityUserId)
+    {
+        return await _appDbContext.DomainUsers.FirstAsync(u => u.AuthUserId == identityUserId);
+    }
+
+    public async Task UpdateAsync(User user)
+    {
+        _appDbContext.DomainUsers.Update(user);
+        await _appDbContext.SaveChangesAsync();
+    }
+}
