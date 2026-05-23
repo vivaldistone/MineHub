f5203965e6515fdabdade57a6ecb29fb0b07a297
vivaldistone <pipenabsolute@icloud.com>
2026-05-23 19:24:36 +0300

    refactor(seeders) : add product seeder in orchestrator

--- src/MineHub.Infrastructure/Persistence/Seeders/DatabaseSeeder.cs
+++ src/MineHub.Infrastructure/Persistence/Seeders/DatabaseSeeder.cs
@@ -4,16 +4,19 @@ public class DatabaseSeeder
 {
     private readonly IdentitySeeder _identitySeeder;
     private readonly RoleSeeder _roleSeeder;
+    private readonly ProductSeeder _productSeeder;
 
-    public DatabaseSeeder(IdentitySeeder identitySeeder, RoleSeeder roleSeeder)
+    public DatabaseSeeder(IdentitySeeder identitySeeder, RoleSeeder roleSeeder, ProductSeeder productSeeder)
     {
         _identitySeeder = identitySeeder;
         _roleSeeder = roleSeeder;
+        _productSeeder = productSeeder;
     }
 
     public async Task SeedAsync()
     {
         await _identitySeeder.SeedAsync();
         await _roleSeeder.SeedAsync();
+        await _productSeeder.SeedAsync();
     }
 }
