2d27eb3f8304348d0ac419d7f40875762923a91a
vivaldistone <pipenabsolute@icloud.com>
2026-05-23 19:22:48 +0300

    feat(seeders) : add product seeder

---
+++ src/MineHub.Infrastructure/Persistence/Seeders/ProductSeeder.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MineHub.Domain.Entities;
+
+namespace MineHub.Infrastructure.Persistence.Seeders;
+
+public class ProductSeeder
+{
+    private readonly AppDbContext _appDbContext;
+
+    public ProductSeeder(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task SeedAsync()
+    {
+        var premium = new Product("premium", "premium access", 100);
+        var sword = new Product("sword", "sword", 250);
+
+        if (!await _appDbContext.Products.AnyAsync(p => p.Name =="premium"))
+        {
+            await _appDbContext.Products.AddAsync(premium);
+        }
+
+        if(!await _appDbContext.Products.AnyAsync(p => p.Name == "sword"))
+        {
+            await _appDbContext.Products.AddAsync(sword);
+        }
+
+        await _appDbContext.SaveChangesAsync();
+    }
+}
