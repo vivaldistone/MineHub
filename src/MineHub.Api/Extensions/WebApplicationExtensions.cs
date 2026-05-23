842649070e8799dd05073a2180490d2371199119
vivaldistone <pipenabsolute@icloud.com>
2026-05-23 19:31:38 +0300

    feat(WebApplication) : add web application extension

---
+++ src/MineHub.Api/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using MineHub.Infrastructure.Persistence.Seeders;
+
+namespace MineHub.Api.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static async Task SeedDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var seeder = scope.ServiceProvider
+            .GetRequiredService<DatabaseSeeder>();
+
+        await seeder.SeedAsync();
+    }
+}
