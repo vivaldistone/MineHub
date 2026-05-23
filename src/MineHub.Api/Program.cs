56617c8fe05805903be173ea87b4b2b00c4fdcff
vivaldistone <pipenabsolute@icloud.com>
2026-05-23 19:33:35 +0300

    feat(Program) : add controllers, swagger, db context, identity

--- src/MineHub.Api/Program.cs
+++ src/MineHub.Api/Program.cs
@@ -1,5 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using MineHub.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using MineHub.Infrastructure.Identity;
+using MineHub.Api.Extensions;
+using MineHub.Infrastructure.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplication();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(builderOptions => builderOptions.UseNpgsql(connectionString));
+
+builder.Services.AddIdentityCore<AuthUser>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+}).AddRoles<IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+await app.SeedDatabaseAsync();
+
+app.MapControllers();
 
 app.Run();
