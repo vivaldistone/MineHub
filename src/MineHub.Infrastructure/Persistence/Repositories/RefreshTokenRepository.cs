b2e3f2c942fce8b229f6f4686c09f976ebac3a27
vivaldistone <pipenabsolute@icloud.com>
2026-05-28 15:41:42 +0300

    feat(RefreshTokenRepository) : add repository for refresh token

---
+++ src/MineHub.Infrastructure/Persistence/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MineHub.Application.Abstractions.Persistence;
+using MineHub.Domain.Entities;
+
+namespace MineHub.Infrastructure.Persistence.Repositories;
+
+public class RefreshTokenRepository : IRefreshTokenRepository
+{
+    private readonly AppDbContext _appDbContext;
+
+    public RefreshTokenRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task AddAsync(RefreshToken refreshToken)
+    {
+        await _appDbContext.RefreshTokens.AddAsync(refreshToken);
+        await _appDbContext.SaveChangesAsync();
+    }
+
+    public async Task<RefreshToken> GetRefreshTokenAsync(string hash)
+    {
+        return await _appDbContext.RefreshTokens
+            .FirstAsync(token => token.HashToken == hash);
+    }
+
+    public async Task UpdateAsync(RefreshToken token)
+    {
+        _appDbContext.Update(token);
+        await _appDbContext.SaveChangesAsync();
+    }
+}
