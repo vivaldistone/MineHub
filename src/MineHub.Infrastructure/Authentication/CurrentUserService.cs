626e504d450e20a6d38e5e173cfee4af0a2a7d0c
vivaldistone <pipenabsolute@icloud.com>
2026-05-28 15:44:34 +0300

    feat(CurrentUserService) : add current user service from IHttpContextAccessor  service

---
+++ src/MineHub.Infrastructure/Authentication/CurrentUserService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using MineHub.Application.Abstractions.Services;
+using System.Security.Claims;
+
+namespace MineHub.Infrastructure.Authentication;
+
+public class CurrentUserService : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+
+    public CurrentUserService(IHttpContextAccessor contextAccessor)
+    {
+        _contextAccessor = contextAccessor;
+    }
+
+    public string? UserId => _contextAccessor?.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+}
