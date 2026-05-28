0e8f6f199f279d2b95d8b049759072abfe55c98a
vivaldistone <pipenabsolute@icloud.com>
2026-05-28 15:50:36 +0300

    refactor(DependencyInjection) : fix bugs dependency injection  class for infrastructure layer

--- src/MineHub.Infrastructure/DependencyInjection/DependencyInjection.cs
+++ src/MineHub.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using MineHub.Application.Abstractions.Persistence;
-using MineHub.Application.Products.Commands.AddProduct;
-using MineHub.Application.Products.Commands.DeleteProduct;
-using MineHub.Application.Products.Commands.UpdateProduct;
-using MineHub.Application.Products.Queries.GetProduct;
-using MineHub.Application.Products.Queries.GetProducts;
+using MineHub.Application.Abstractions.Services;
+using MineHub.Infrastructure.Authentication;
+using MineHub.Infrastructure.Identity.Services;
 using MineHub.Infrastructure.Persistence.Repositories;
 using MineHub.Infrastructure.Persistence.Seeders;
 
@@ -12,23 +11,21 @@ namespace MineHub.Infrastructure.DependencyInjection;
 
 public static class DependencyInjection 
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddInfrastucture(this IServiceCollection services)
     {
-        services.AddScoped<GetProductsQueryHandler>();
-        services.AddScoped<GetProductQueryHandler>();
-        services.AddScoped<AddProductCommandHandler>();
-        services.AddScoped<DeleteProductCommandHandler>();
-        services.AddScoped<UpdateProductCommandHandler>();
-
-
-
-
-
+        services.AddScoped<ICartRepository, CartRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+
+        services.AddScoped<ICurrentUserService, CurrentUserService>();
+        services.AddScoped<ICurrentDomainUserService, DomainUserService>();
         services.AddScoped<DatabaseSeeder>();
         services.AddScoped<ProductSeeder>();
         services.AddScoped<RoleSeeder>();
         services.AddScoped<IdentitySeeder>();
+
+        services.AddHttpContextAccessor();
 
         return services;
     }
