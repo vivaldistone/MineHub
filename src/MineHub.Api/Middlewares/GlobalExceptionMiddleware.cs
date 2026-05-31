84b5484950858e52983a03b1b8bdfa51a0c23e04
vivaldistone <pipenabsolute@icloud.com>
2026-05-31 22:11:36 +0300

    feat(GlobalExceptionMiddleware) : add global exception middleware

---
+++ src/MineHub.Api/Middlewares/GlobalExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using MineHub.Application.Exceptions;
+using MineHub.Domain.Exceptions;
+using System.Text.Json;
+
+namespace MineHub.Api.Middlewares;
+
+public class GlobalExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public GlobalExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch(Exception exception)
+        {   
+            var statusCode = exception switch
+            { 
+                DomainException => StatusCodes.Status400BadRequest,
+                NotFoundException => StatusCodes.Status404NotFound,
+                ConflictException => StatusCodes.Status409Conflict,
+                InvalidCredentialsException => StatusCodes.Status401Unauthorized,
+                UnauthorizedException => StatusCodes.Status401Unauthorized,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            context.Response.StatusCode = statusCode;
+            context.Response.ContentType = "application/json";
+
+            var response = new
+            {
+                statusCode,
+                message = exception.Message,
+            };
+
+            var json = JsonSerializer.Serialize(response);
+
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
