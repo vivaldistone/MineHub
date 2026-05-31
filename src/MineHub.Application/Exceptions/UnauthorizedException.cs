c27e30ac300a4a7fdf5a8af2428fc26d1e5a10a7
vivaldistone <pipenabsolute@icloud.com>
2026-05-31 21:49:29 +0300

    feat(UnauthorizedException) : add unauthorized exception

---
+++ src/MineHub.Application/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,10 @@
+﻿namespace MineHub.Application.Exceptions;
+
+public class UnauthorizedException : Exception
+{
+    public string Code { get; private set; }
+    public UnauthorizedException(string message, string code) : base(message)
+    {
+        Code = code;
+    }
+}
