01673ef2b7c41efd67ee0f458b47014b56cc5fde
vivaldistone <pipenabsolute@icloud.com>
2026-06-25 20:02:03 +0300

    feat(persistence) : add unit of work pattern

---
+++ src/MineHub.Application/Abstractions/Persistence/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace MineHub.Application.Abstractions.Persistence;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken token);
+}
---
+++ src/MineHub.Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using MineHub.Application.Abstractions.Persistence;
+
+namespace MineHub.Infrastructure.Persistence.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<int> SaveChangesAsync(CancellationToken token)
+    {
+        return _context.SaveChangesAsync(token);
+    }
+}
