c0086cbeed58efacbf694ab32986e77516834355
vivaldistone <pipenabsolute@icloud.com>
2026-07-05 19:49:46 +0300

    feat(EmalSender) : add email sender abstractions service

---
+++ src/MineHub.Application/Abstractions/Email/IEmailSender.cs
@@ -0,0 +1,6 @@
+﻿namespace MineHub.Application.Abstractions.Email;
+
+public interface IEmailSender
+{
+    Task SendEmailAsync(string to, string subject, string htmlBody, CancellationToken token);
+}
