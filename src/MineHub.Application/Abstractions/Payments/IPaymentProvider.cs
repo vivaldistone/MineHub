52ba1a21b23883687a3aa5cd7669cace5b4acd74
vivaldistone <pipenabsolute@icloud.com>
2026-07-05 19:48:48 +0300

    feat(Payment) : add IPaymentProvider abstractions service

---
+++ src/MineHub.Application/Abstractions/Payments/IPaymentProvider.cs
@@ -0,0 +1,8 @@
+﻿using MineHub.Application.Payments.Commands.CreatePayment;
+
+namespace MineHub.Application.Abstractions.Payments;
+
+public interface IPaymentProvider
+{
+    Task<CreatePaymentProviderResult> CreatePaymentAsync(Guid orderId, Guid paymentId, decimal amount);
+}
