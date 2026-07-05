f37abe087f994bd0ba09adab383062d960607068
vivaldistone <pipenabsolute@icloud.com>
2026-07-05 20:10:20 +0300

    feat(UnitOfWork) : Add UOW

---
+++ src/MineHub.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using MineHub.Application.Abstractions.Persistence;
+
+namespace MineHub.Infrastructure.Persistence;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<int> SaveChangesAsync(CancellationToken token)
+    {
+        return _context.SaveChangesAsync(token);
+    }
+}
