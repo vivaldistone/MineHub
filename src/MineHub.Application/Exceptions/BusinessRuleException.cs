ff50f5c713a8f24237485d8d0ceff38a1ed991ab
vivaldistone <pipenabsolute@icloud.com>
2026-07-05 19:52:23 +0300

    feat(exceptions) : add business rule exception

---
+++ src/MineHub.Application/Exceptions/BusinessRuleException.cs
@@ -0,0 +1,9 @@
+﻿namespace MineHub.Application.Exceptions;
+
+public sealed class BusinessRuleException : ApplicationException
+{
+    public BusinessRuleException(string message, string code) 
+        : base(message, code)
+    {
+    }
+}
