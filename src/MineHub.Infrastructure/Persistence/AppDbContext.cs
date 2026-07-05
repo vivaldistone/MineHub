60cc878a626f350a2c6bbe2d105d01eaa9d04fa9
vivaldistone <pipenabsolute@icloud.com>
2026-07-05 20:09:14 +0300

    refactor(AppDbContext) : refactor

--- src/MineHub.Infrastructure/Persistence/AppDbContext.cs
+++ src/MineHub.Infrastructure/Persistence/AppDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MineHub.Domain.Entities;
-using MineHub.Infrastructure.Identity;
+using MineHub.Infrastructure.Auth.Entities;
 
 namespace MineHub.Infrastructure.Persistence;
 
@@ -9,10 +9,9 @@ public class AppDbContext : IdentityDbContext<AuthUser>
 {
     public DbSet<Product> Products => Set<Product>();
     public DbSet<User> DomainUsers => Set<User>();
-    public DbSet<Cart> Carts => Set<Cart>();
     public DbSet<Order> Orders => Set<Order>();
-
     public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+    public DbSet<Payment> Payments => Set<Payment>();
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
     {
